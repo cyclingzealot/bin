083309dca62235c9d7601b0efdd847c829f102bf
Julien Lamarche <jlam@credil.org>
2013-12-18 16:56:55 -0500

    adding validateJSON.bash

--- mybashrc.bash
+++ mybashrc.bash
@@ -6,3 +6,9 @@ export PATH=$PATH:~/bin/
 . ~/bin/ps1git.bash
 . ~/bin/.git-prompt.sh
 
+
+export HOSTNAME=`hostname` # HOSTNAME not set some machines
+if [ -x /usr/bin/keychain -a -f $HOME/.keychain/${HOSTNAME}-sh ] ; then
+/usr/bin/keychain --quiet --nogui $HOME/.ssh/id_cityOttawaGitHub
+source $HOME/.keychain/${HOSTNAME}-sh
+fi
