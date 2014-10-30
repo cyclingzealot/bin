4fe69783967258a4572c92ba7666416e4552563c
Julien Lamarche <julien.lamarche@gmail.com>
2014-10-30 17:52:52 -0400

    added pack count and pct to upgrade.bash - not fully tested

--- upgrade.bash
+++ upgrade.bash
@@ -4,13 +4,24 @@ sudo /usr/local/bin/composer self-update
 sudo apt-get update
 sudo apt-get upgrade --just-print | grep Inst | cut -f 2 -d ' ' | sort  > /tmp/upgradePackageList.txt
 
+numPacks=`wc -l /tmp/upgradePackageList.txt`
+
 continueFlag=/tmp/continueUpgrade
+i="0"
+
 touch $continueFlag
 for pack in `cat /tmp/upgradePackageList.txt` ; do 
-	echo $pack
+	set -x
+	i=`echo "$i + 1" | bc`
+	pct=`echo "$i / $numPacks *100" | bc`
+
+
+	echo "$pack ($i of $numPacks - $pct %)"
+	set +x
 	echo ===== To stop =======\> rm $continueFlag
 	if [ -a $continueFlag ] ; then sudo apt-get install $pack --only-upgrade --yes  -d; fi
 	if [ -a $continueFlag ] ; then sudo apt-get install $pack --only-upgrade --yes ;  fi
+	sleep 1
 	echo 
 done
 
