b99378f2138c1215c8e98e568cc88cdeb604b930
Julien Lamarche <julien.lamarche@gmail.com>
2014-11-11 13:57:15 -0500

    fixed pct calculations

--- upgrade.bash
+++ upgrade.bash
@@ -4,16 +4,15 @@ sudo /usr/local/bin/composer self-update
 sudo apt-get update
 sudo apt-get upgrade --just-print | grep Inst | cut -f 2 -d ' ' | sort  > /tmp/upgradePackageList.txt
 
-numPacks=`wc -l /tmp/upgradePackageList.txt`
+numPacks=`wc -l /tmp/upgradePackageList.txt | cut -f 1 -d ' '`
 
 continueFlag=/tmp/continueUpgrade
 i="0"
 
 touch $continueFlag
 for pack in `cat /tmp/upgradePackageList.txt` ; do 
-	set -x
 	i=`echo "$i + 1" | bc`
-	pct=`echo "$i / $numPacks *100" | bc`
+	pct=`echo "$i / $numPacks * 100" | bc`
 
 
 	echo "$pack ($i of $numPacks - $pct %)"
