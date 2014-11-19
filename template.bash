9341c87ba555ca78d9de8e71143fcd16a73e82a6
vagrant <vagrant@localdev.spotlight.ottawa.ca>
2014-11-19 13:52:33 -0800

    added template

---
+++ template.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#exit when command fails (use || true when a command can fail)
+set -o errexit
+
+#exit when your script tries to use undeclared variables
+set -o nounset
+
+#(a.k.a set -x) to trace what gets executed
+set -o xtrace
+
+# in scripts to catch mysqldump fails 
+set -o pipefail
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
