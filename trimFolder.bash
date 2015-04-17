d0fc869a846bcf71e9df6bef2890f02faba3ce39
Julien Lamarche <julien.lamarche@gmail.com>
2015-04-17 16:09:44 -0400

    added trimFolder

---
+++ trimFolder.bash
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+START=$(date +%s.%N)
+
+#Set the config file
+configFile='~/.binJlam/templateConfig'
+
+#exit when command fails (use || true when a command can fail)
+set -o errexit
+
+#exit when your script tries to use undeclared variables
+set -o nounset
+
+#(a.k.a set -x) to trace what gets executed
+set -o xtrace
+
+# in scripts to catch mysqldump fails 
+set -o pipefail
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+ts=`date +'%Y%m%d-%H%M%S'`
+
+
+#Capture everything to log
+log=~/log/$__base-${ts}.log
+exec >  >(tee -a $log)
+exec 2> >(tee -a $log >&2)
+
+
+#Check that the config file exists
+#if [[ ! -f "$configFile" ]] ; then
+#        echo "I need a file at $configFile with ..."
+#        exit 1
+#fi
+
+
+echo Begin `date`  .....
+
+### BEGIN SCRIPT ###############################################################
+
+# Author 
+# that other guy 
+# http://stackoverflow.com/questions/25514434/bash-script-to-keep-deleting-files-until-directory-size-is-less-than-x#answer-25514993
+
+target=$1
+maxsize=$2  # In MB
+
+while [ "$(du -shm $target | awk '{print $1}')" -gt $maxsize ]
+do
+  find $target -maxdepth 1 -type f -printf '%T@\t%p\n' | \
+      sort -n | head -n 25 | cut -d $'\t' -f 2-  | xargs -d '\n' rm -vif
+done
+
+
+### END SCIPT ##################################################################
+
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo Done.  `date` - $DIFF seconds
