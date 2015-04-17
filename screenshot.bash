9217b457b85d9d59dcc33bed1d23053aca67d1ef
Julien Lamarche <julien.lamarche@gmail.com>
2015-04-17 15:51:03 -0400

    using date instead of scrot placeholders for file name

--- screenshot.bash
+++ screenshot.bash
@@ -2,10 +2,9 @@
 #set -x
 whoami=`whoami`
 dest=/home/$whoami/screenshots/
-file=/home/$whoami/$whoami-screenshot-%Y-%m-%d-%H_%M.png
-filemv=/home/$whoami/$whoami-screenshot-*.png
+file=/home/$whoami/$whoami-screenshot-`date +'%Y-%m-%d-%H-%M-%S'`.png
 mkdir -p $dest
 DISPLAY=:0 /usr/bin/scrot "$file"
-mv -v $filemv $dest
+mv -v $file $dest
 find $dest -name '*png' -type f -mtime +30 -exec rm -v {} \;
 
