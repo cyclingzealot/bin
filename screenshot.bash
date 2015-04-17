b8bcc01d026001beb5f112d7d9dfa9fc2e336c16
Julien Lamarche <julien.lamarche@gmail.com>
2015-04-17 15:54:14 -0400

    added scrott documentation

--- screenshot.bash
+++ screenshot.bash
@@ -1,5 +1,10 @@
 #!/bin/sh
 #set -x
+
+#Desgined to run in a crontab
+#such as 
+#*/3 * * * * /home/jlam/bin/screenshot.bash 2> ~/log/screenshot.crontab.log 2>&1
+
 whoami=`whoami`
 dest=/home/$whoami/screenshots/
 file=/home/$whoami/$whoami-screenshot-`date +'%Y-%m-%d-%H-%M-%S'`.png
