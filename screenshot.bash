9a2ca212f5fd3133fbe294a70710456f09afaa65
Julien Lamarche <julien.lamarche@gmail.com>
2015-04-17 16:14:06 -0400

    wrapped screenshot script in stardard template

--- screenshot.bash
+++ screenshot.bash
@@ -1,5 +1,46 @@
-#!/bin/sh
-#set -x
+#!/usr/bin/env bash
+
+START=$(date +%s.%N)
+
+#Set the config file
+configFile='~/.binJlam/templateConfig'
+
+#exit when command fails (use || true when a command can fail)
+set -o errexit
+
+#exit when your script tries to use undeclared variables
+set -o nounset
+
+#(a.k.a set -x) to trace what gets executed
+set -o xtrace
+
+# in scripts to catch mysqldump fails 
+set -o pipefail
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+ts=`date +'%Y%m%d-%H%M%S'`
+
+
+#Capture everything to log
+log=~/log/$__base-${ts}.log
+exec >  >(tee -a $log)
+exec 2> >(tee -a $log >&2)
+
+
+#Check that the config file exists
+#if [[ ! -f "$configFile" ]] ; then
+#        echo "I need a file at $configFile with ..."
+#        exit 1
+#fi
+
+
+echo Begin `date`  .....
+
+### BEGIN SCRIPT ###############################################################
 
 #Desgined to run in a crontab
 #such as 
@@ -29,3 +70,8 @@ do
       sort -n | head -n 25 | cut -d $'\t' -f 2-  | xargs -d '\n' rm -v
 done
 
+### END SCIPT ##################################################################
+
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo Done.  `date` - $DIFF seconds
