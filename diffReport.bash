cf3ceb5655421ff0a99b991addd862f65633a96a
Julien Lamarche <julien.lamarche@gmail.com>
2015-04-27 17:05:38 -0400

    added diffReport.bash

---
+++ diffReport.bash
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+START=$(date +%s.%N)
+
+#exit when command fails (use || true when a command can fail)
+set -o errexit
+
+#exit when your script tries to use undeclared variables
+set -o nounset
+
+#(a.k.a set -x) to trace what gets executed
+#set -o xtrace
+
+# in scripts to catch mysqldump fails 
+set -o pipefail
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+ts=`date +'%Y%m%d-%H%M%S'`
+
+#Set the config file
+configFile='~/.binJlam/templateConfig'
+
+
+#Capture everything to log
+log=~/log/$__base-${ts}.log
+exec >  >(tee -a $log)
+exec 2> >(tee -a $log >&2)
+
+
+#Check that the config file exists
+#if [[ ! -f "$configFile" ]] ; then
+#        echo "I need a file at $configFile with ..."
+#        exit 1
+#fi
+
+
+echo Begin `date`  .....
+
+### BEGIN SCRIPT ###############################################################
+
+topPath=`git rev-parse --show-toplevel`
+prjName=`basename $topPath`
+whoami=`whoami`
+
+reportPath=/tmp/$whoami-$prjName-$ts.diff
+
+git diff | tee $reportPath
+
+echo ; echo ; echo
+
+echo Report in:
+echo 
+echo $reportPath
+
+echo ; echo ; echo
+
+
+### END SCIPT ##################################################################
+
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo Done.  `date` - $DIFF seconds
