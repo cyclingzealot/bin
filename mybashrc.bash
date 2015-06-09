863b8dcc3f4d20db881159a96ebe8632706e9b5a
Julien Lamarche <julien.lamarche@gmail.com>
2015-06-09 13:48:16 -0400

    modified usage alias

--- mybashrc.bash
+++ mybashrc.bash
@@ -8,17 +8,11 @@ export PATH=$PATH:~/bin/local/
 . ~/bin/.git-prompt.sh
 
 
-export HOSTNAME=`hostname` # HOSTNAME not set some machines
-if [ -x /usr/bin/keychain -a -f $HOME/.keychain/${HOSTNAME}-sh ] ; then
-/usr/bin/keychain --quiet --nogui $HOME/.ssh/id_cityOttawaGitHub
-source $HOME/.keychain/${HOSTNAME}-sh
-fi
-
 alias parseMultiLineInsert="sed \'s$),($),\n($g\'"
 
 export BC_ENV_ARGS=~/.bcrc
 
-alias usage='find . -maxdepth 1 -print0 | xargs -0 du -s | sort -n -r -k 1'
+alias usage='find .  -maxdepth 1 -print0 | xargs -0 du -aS | sort -n -r -k 1'
 alias usageFiilesOnly='find . -maxdepth 3 -type f -print0 | xargs -0 du | sort -nrk 1'
 
 alias mv='mv -i'
