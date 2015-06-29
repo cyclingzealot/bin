e8bd403dab155cb9ab61322799fac091095696ad
Julien Lamarche <jlam@credil.org>
2015-06-29 12:11:15 -0400

    countdown.bash can now count up

--- countdown.bash
+++ countdown.bash
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-for i in `seq $1 -1 0`; do printf "$i... " ; sleep 1; done
+if [[ $1 != 'up' ]]; then
+	 for i in `seq $1 -1 0`; do printf "$i... " ; sleep 1; done
+else 
+	i=0
+	 while(true); do printf "$i... " ; sleep 1; let i++; done
+fi
