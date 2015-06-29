fcf2b31084753a3078dbef27f88e70437ed98f8f
Julien Lamarche <jlam@credil.org>
2015-06-29 13:06:36 -0400

    counts up if no number

--- countdown.bash
+++ countdown.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-if [[ $1 != 'up' ]]; then
+if [[ $1 =~ '^[0-9]+$' ]]; then
 	 for i in `seq $1 -1 0`; do printf "$i... " ; sleep 1; done
 	exit 0
 fi
 
-echo 'No argument specified... counting up....'
+echo 'No number specified... counting up....'
 i=0
 while(true); do printf "$i... " ; sleep 1; let i++; done
