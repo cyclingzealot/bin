68662c5a6c90a102bee06a008658ee6a4d26303f
Julien Lamarche <jlam@credil.org>
2015-06-29 12:13:15 -0400

    count up if no argument specified

--- countdown.bash
+++ countdown.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+
 if [[ $1 != 'up' ]]; then
 	 for i in `seq $1 -1 0`; do printf "$i... " ; sleep 1; done
-else 
-	i=0
-	 while(true); do printf "$i... " ; sleep 1; let i++; done
+	exit 0
 fi
+
+echo 'No argument specified... counting up....'
+i=0
+while(true); do printf "$i... " ; sleep 1; let i++; done
