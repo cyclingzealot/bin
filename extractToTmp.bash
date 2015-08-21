728af146f641c7ceaf0079c078ef32384c44a13a
Julien Lamarche <julien.lamarche@gmail.com>
2015-08-21 14:43:54 -0400

    added prompt for not overwritting files in extractToTmp

--- extractToTmp.bash
+++ extractToTmp.bash
@@ -3,7 +3,20 @@
 # Extracts all the *.gz files to tmp
 
 for file in *.gz ; do 
-    target=`echo $file | rev | cut -d '.' -f 2- | rev` ; 
-    gzip -dcv $file > /tmp/$target; 
-    ls -l  /tmp/$target;  
+    target=/tmp/`echo $file | rev | cut -d '.' -f 2- | rev` ; 
+
+    if [ -f $target ]; then 
+    
+        ls -lh $target;  
+        read -p "Do you wish to overwrite this file? (Y/N) " yn
+        
+        if [[ ! "$yn" =~ ^[Yy] ]]; then
+            echo Answer did not contain Y or y.  Not overwritting.
+            continue        
+        fi
+    
+    fi
+
+    gzip -dcv $file > $target; 
+    ls -lh $target;  
 done
