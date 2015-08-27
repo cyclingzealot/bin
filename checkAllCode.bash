0d6cee67ee9cac156a3218def5598f6acd75ba81
Julien Lamarche <julien.lamarche@gmail.com>
2015-08-26 21:32:52 -0400

    added checkAllCode

---
+++ checkAllCode.bash
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+currentPWD=`pwd`;  for dir in `find ~/code -maxdepth 2 -name .git  -type d`; do echo `dirname $dir` ; cd `dirname $dir` ; git-branch-status.bash; git submodule foreach --recursive git-branch-status.bash;  done; cd $currentPWD;
+
