e59def3bd9ce3812d6b0591fcbd1fe5d385a9c66
Julien Lamarche <julien.lamarche@gmail.com>
2015-10-15 08:01:29 -0400

    checkAllCode now prepends submodules with spacing

--- checkAllCode.bash
+++ checkAllCode.bash
@@ -6,6 +6,6 @@ for dir in `find ~/bin ~/code/ -maxdepth 2 -name .git  -type d`; do
 	echo `dirname $dir` ; 
 	cd `dirname $dir` ; 
 	git-branch-status.bash; 
-	git submodule foreach --recursive git-branch-status.bash;  
+	git submodule foreach --recursive git-branch-status.bash | sed -e "s/^/    /"
 done; cd $currentPWD;
 
