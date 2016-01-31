30f13fbb3ba21629af5f8435be07198af26ef447
Julien Lamarche <julien.lamarche@gmail.com>
2016-01-31 18:00:41 -0500

    added documentation  to monthCheck.bash

--- monthCheck.bash
+++ monthCheck.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Goes through all pdf files, looking for date formats 
+#   shortMonth date, year
+# Transforms those dates into YYYYMM, sort and uniqs them.  
+# You can then check if you have all months in your bills.
+
+
 IFS=$'\n'
 
 for file in ./*.pdf ; do  
@@ -10,3 +16,6 @@ done  | grep 201 | sort | uniq | grep -oe '[A-Z][a-z]\{2\} [0-9]\{2\}, [0-9]\{4\
 for mois in `cat /tmp/datesFound.txt` ; do
     date -d"$mois" +'%Y%m'
 done | sort | uniq
+
+
+rm /tmp/datesFound.txt
