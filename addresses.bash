c57b5a66df69645f8e9d7f13e40fa3905abee251
Julien Lamarche <julien.lamarche@gmail.com>
2016-03-17 10:46:32 -0400

    addapted addresses.bash to multiples formats

--- addresses.bash
+++ addresses.bash
@@ -3,6 +3,11 @@
 # Works for IPV4
 
 for interface in `ifconfig | grep -o ^[a-z]*` ; do
-    addr=`ifconfig $interface | grep -i inet |  grep -i adr | head -n 1 | cut -d ':' -f 2 | cut -d ' ' -f 1`
+    addr=`ifconfig $interface | grep -i inet |  grep 'adr\|addr' | head -n 1 | cut -d ':' -f 2 | cut -d ' ' -f 1`
+
+    if [ -z "$addr" ]; then  # Try IPv6
+        addr=`ifconfig $interface | grep -i inet |  grep 'adr\|addr' | head -n 1 | cut -d ':' -f 2- | cut -d ' ' -f 2`
+    fi
+
     printf "%10s: %s\n" $interface $addr
 done
