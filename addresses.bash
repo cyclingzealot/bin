5c58dabb2ba86cda7ef31eadc95723a4210b2bab
Julien Lamarche <julien.lamarche@gmail.com>
2016-03-17 10:49:23 -0400

    slight modification to comment

--- addresses.bash
+++ addresses.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Works for IPV4
-
+# Prioritizes ipv4.
 for interface in `ifconfig | grep -o ^[a-z]*` ; do
     addr=`ifconfig $interface | grep -i inet |  grep 'adr\|addr' | head -n 1 | cut -d ':' -f 2 | cut -d ' ' -f 1`
 
