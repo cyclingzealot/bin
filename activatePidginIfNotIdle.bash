3b6190bda825b0baea3a58c0c60507712683b29a
Julien Lamarche <julien.lamarche@gmail.com>
2016-04-01 01:50:49 -0400

    added activatePidginIfNotIdle.bash

---
+++ activatePidginIfNotIdle.bash
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+START=$(date +%s.%N)
+
+#exit when command fails (use || true when a command can fail)
+set -o errexit
+
+#exit when your script tries to use undeclared variables
+set -o nounset
+
+#(a.k.a set -x) to trace what gets executed
+#set -o xtrace
+
+# in scripts to catch mysqldump fails
+set -o pipefail
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+ts=`date +'%Y%m%d-%H%M%S'`
+
+#Set the config file
+configFile="$HOME/.binJlam/templateConfig"
+
+
+#Capture everything to log
+log=~/log/$__base-${ts}.log
+exec >  >(tee -a $log)
+exec 2> >(tee -a $log >&2)
+touch $log
+chmod 600 $log
+
+
+#Check that the config file exists
+#if [[ ! -f "$configFile" ]] ; then
+#        echo "I need a file at $configFile with ..."
+#        exit 1
+#fi
+
+
+echo Begin `date`  .....
+
+### BEGIN SCRIPT ###############################################################
+
+export DISPLAY=:0
+idleTime=`xprintidle`
+
+echo Idle time is $idleTime
+
+pidginInstances=`ps -ef | grep pidgin | grep -v defunct | grep -v grep | wc -l`
+
+echo There are $pidginInstances pidgin instances
+
+if [[ "$pidginInstances" -eq 0 -a "$idleTime" -lt 600000 ]]; then
+	echo "Activating pidgin"
+	pidgin &
+else
+	echo "No need to activate pidgin"
+fi
+
+
+
+### END SCIPT ##################################################################
+
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo Done.  `date` - $DIFF seconds
