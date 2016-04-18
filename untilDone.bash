13b8bb83ebc6adf7b03eada744d347b246784582
Julien Lamarche <julien.lamarche@gmail.com>
2016-04-18 15:36:10 -0400

    added optional wait time in untilDone.bash

--- untilDone.bash
+++ untilDone.bash
@@ -2,6 +2,7 @@
 
 
 numProcs=1
+waitSecs=${2:-1}
 
 while [[ "$numProcs" -gt 0 ]]; do
 
@@ -9,12 +10,12 @@ while [[ "$numProcs" -gt 0 ]]; do
 #set -x
 numProcs=`ps -ef | grep $1 | grep -v grep | grep -v untilDone | wc -l`
 
-if [[ "$numProcs" -gt 0 ]]; then 
-	printf . ; 
-	sleep 1; 
-	#echo ; 
-	#ps -ef | grep $1 ; 
-	#echo ; 
+if [[ "$numProcs" -gt 0 ]]; then
+	printf . ;
+	sleep $waitSecs;
+	#echo ;
+	#ps -ef | grep $1 ;
+	#echo ;
 fi
 
 
