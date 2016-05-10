2301893aed48198d021ffe3fa2e90eaf2965959e
Julien Lamarche <julien.lamarche@gmail.com>
2016-05-10 08:01:27 -0400

    added templateSmall.bash

---
+++ templateSmall.bash
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+START=$(date +%s.%N)
+
+arg1=${1:-''}
+
+if [[ $arg1 == '--help' || $arg1 == '-h' ]]; then
+    echo "Script author should have provided documentation"
+    exit 0
+fi
+
+#exit when command fails (use || true when a command can fail)
+set -o errexit
+
+#exit when your script tries to use undeclared variables
+set -o nounset
+
+# in scripts to catch mysqldump fails
+set -o pipefail
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"   # Dir of the script
+__root="$(cd "$(dirname "${__dir}")" && pwd)"           # Dir of the dir of the script
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"       # Full path of the script
+__base="$(basename ${__file})"                          # Name of the script
+ts=`date +'%Y%m%d-%H%M%S'`
+ds=`date +'%Y%m%d'`
+pid=`ps -ef | grep ${__base} | grep -v 'vi ' | head -n1 |  awk ' {print $2;} '`
+formerDir=`pwd`
+
+#Set the config file
+configFile="$HOME/.binJlam/templateConfig"
+
+#Check that the config file exists
+#if [[ ! -f "$configFile" ]] ; then
+#        echo "I need a file at $configFile with ..."
+#        exit 1
+#fi
+
+export DISPLAY=:0
+
+echo Begin `date`  .....
+
+echo; echo; echo;
+
+### BEGIN SCRIPT ###############################################################
+
+#(a.k.a set -x) to trace what gets executed
+set -o xtrace
+
+
+
+set +x
+
+### END SCIPT ##################################################################
+
+cd $formerDir
+
+END=$(date +%s.%N)
+DIFF=$(echo "round($END - $START)" | bc)
+echo; echo; echo;
+echo Done.  `date` - $DIFF seconds
