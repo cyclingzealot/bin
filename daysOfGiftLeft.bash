f4d94f5750127a2db3e134f93a135a234b3e286f
Julien Lamarche <julien.lamarche@gmail.com>
2016-05-12 11:01:01 -0400

    avoid double runs of daysOfGiftLeft

--- daysOfGiftLeft.bash
+++ daysOfGiftLeft.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+
+
+lastRunFile="$HOME/.`basename $0`.lastRun"
+
+if [[ -f $lastRunFile &&  `expr $(date +%s) - $(date +%s -r $lastRunFile)` -lt 4 ]]; then
+    exit 0
+else
+    touch $lastRunFile
+fi
+
+
+
 NAISSANCE=`date -d '1981/07/09 8:13' +'%s'`
 ESP=88
 maintenant=`date +'%s'`
