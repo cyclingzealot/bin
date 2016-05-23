6737c1e9afebbbc3af964cd155386a3b5ba74933
Julien Lamarche <julien.lamarche@gmail.com>
2016-05-23 17:58:18 -0400

    added screenshotSince.bash

---
+++ screenshotSince.bash
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+arg1=${1:-''}
+
+if [[ $arg1 == '--help' || $arg1 == '-h' ]]; then
+    echo "Just specify the time (inclusive) of the first screenshot you wish to have. Time format HH:MM"
+    exit 0
+fi
+
+#exit when command fails (use || true when a command can fail)
+set -o errexit
+
+#exit when your script tries to use undeclared variables
+set -o nounset
+
+# in scripts to catch mysqldump fails
+set -o pipefail
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"   # Dir of the script
+__root="$(cd "$(dirname "${__dir}")" && pwd)"           # Dir of the dir of the script
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"       # Full path of the script
+__base="$(basename ${__file})"                          # Name of the script
+ts=`date +'%Y%m%d-%H%M%S'`
+ds=`date +'%Y%m%d'`
+pid=`ps -ef | grep ${__base} | grep -v 'vi ' | head -n1 |  awk ' {print $2;} '`
+formerDir=`pwd`
+
+export DISPLAY=:0
+
+echo Begin `date`  .....
+
+echo; echo; echo;
+
+### BEGIN SCRIPT ###############################################################
+
+#(a.k.a set -x) to trace what gets executed
+set -o xtrace
+
+since=${1:-}
+
+scDir=$HOME/screenshots/
+
+cd $scDir
+
+sinceTS=`date -d "$since" +'%s'`
+nowTS=`date +'%s'`;
+
+minDiff=`echo "($nowTS - $sinceTS)/60 + 2" | bc | cut -d. -f 1`
+
+noGoodCount=`find $scDir -mmin $minDiff -name '*.nogood.*' | wc -l `
+
+if find $scDir -mmin -$minDiff -name '*.png'; then
+    eog `find $scDir -mmin -$minDiff -name '*.png' ` &
+else
+    echo No file to open
+fi
+
+echo
+echo $noGoodCount no good png files
+echo
+
+
+set +x
+
+### END SCIPT ##################################################################
+
+cd $formerDir
