c4c5a3dafabc1a26f0df411aa788ae2b3dff8af4
Julien Lamarche <jlam@credil.org>
2016-06-09 16:29:48 -0400

    untilFileChange can now take multiple files

---
+++ untilFilesChange.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+files=$@
+set -o pipefail
+oldTS=`stat -c %Y $files | sort -n  | tail -n 1`
+newTS=$oldTS
+
+while [ "$oldTS" -eq "$newTS" ]; do
+    printf . ;
+    newTS=`stat -c %Y $files | sort -n  | tail -n 1`
+    sleep 1
+done
+
+echo
+echo $file changed, proceeding....
