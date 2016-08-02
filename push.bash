2f6f4aa3313ece6a623349c4af4edc4d0514000d
Julien Lamarche <julien.lamarche@gmail.com>
2016-08-02 19:20:37 -0400

    Smarter wait for apt to finish before pushing

--- push.bash
+++ push.bash
@@ -6,12 +6,23 @@ set -o errexit
 #exit when your script tries to use undeclared variables
 set -o nounset
 
+# in scripts to catch mysqldump fails
+set -o pipefail
 
-set -x
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"   # Dir of the script
+__root="$(cd "$(dirname "${__dir}")" && pwd)"           # Dir of the dir of the script
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"       # Full path of the script
+__base="$(basename ${__file})"                          # Name of the script
+ts=`date +'%Y%m%d-%H%M%S'`
+ds=`date +'%Y%m%d'`
+pid=`ps -ef | grep ${__base} | grep -v 'vi ' | head -n1 |  awk ' {print $2;} '`
+formerDir=`pwd`
 
 
 echo Chekcing and waiting until git finishes.....
-untilDone.bash git
+
+untilDone.bash -s git -e "$__base"
 
 ### Commits everything, pulls and, if pull succesfull, pushes
 
