b0853ea0d08e1e1ad3353b9fd60ba619d4989e9a
Julien Lamarche <julien.lamarche@gmail.com>
2016-09-02 17:10:16 -0400

    added  crontabSave.bash

---
+++ crontabSave.bash
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+arg1=${1:-''}
+
+if [[ $arg1 == '--help' || $arg1 == '-h' ]]; then
+    echo "Script author should have provided documentation"
+    exit 0
+fi
+
+#exit when command fails (use || true when a command can fail)
+set -o errexit
+
+#exit when your script tries to use undeclared variables
+set -o nounset
+
+# in scripts to catch mysqldump fails
+set -o pipefail
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"   # Dir of the script
+__root="$(cd "$(dirname "${__dir}")" && pwd)"           # Dir of the dir of the script
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"       # Full path of the script
+__base="$(basename ${__file})"                          # Name of the script
+ts=`date +'%Y%m%d-%H%M%S'`
+ds=`date +'%Y%m%d'`
+pid=`ps -ef | grep ${__base} | grep -v 'vi ' | head -n1 |  awk ' {print $2;} '`
+formerDir=`pwd`
+
+# If you require named arguments, see
+# http://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+
+export DISPLAY=:0
+
+echo "#Saved on `date`"
+
+echo; echo; echo;
+
+### BEGIN SCRIPT ###############################################################
+
+crontab -l
+
+### END SCIPT ##################################################################
+
+cd $formerDir
