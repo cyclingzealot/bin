09adbc558a39f9787a157a92994badfc055aece3
Julien Lamarche <julien.lamarche@gmail.com>
2016-10-10 16:11:20 -0400

    Looks now for french modifié

--- gitListModifiedFiles.bash
+++ gitListModifiedFiles.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git status | grep modified  | awk '{print $3}' | rev | cut -d/ -f 1 | rev | tr $'\n' ' '
+git status | grep -E 'modified\|modifié'  | awk '{print $3}' | rev | cut -d/ -f 1 | rev | tr $'\n' ' '
