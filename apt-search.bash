ce4cd3f9fe16d9a6e648977a73cf4bd09659e75f
Julien Lamarche <julien.lamarche@gmail.com>
2017-09-23 19:27:08 -0400

    added  apt-search.bash

---
+++ apt-search.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#exit when command fails (use || true when a command can fail)
+set -o errexit
+
+#exit when your script tries to use undeclared variables
+set -o nounset
+
+#(a.k.a set -x) to trace what gets executed
+#set -o xtrace
+
+# in scripts to catch mysqldump fails 
+set -o pipefail
+
+apt-cache search $1 | grep $1 | sort | less -niSFX
