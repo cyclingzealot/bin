cced2b37c6c5fec21195b9f2ebd09c884c87840f
Julien Lamarche <julien.lamarche@gmail.com>
2018-03-31 03:56:34 -0400

    finished railsMegaGenerate

--- railsMegaGenerate.rb
+++ railsMegaGenerate.rb
@@ -1,12 +1,29 @@
-# Abandonned, realized I'm probably planning ahead too much
 require 'csv'
+require 'byebug'
 
 filePath=ARGV[0]
 
+if not File.exists?(filePath)
+    $stderr.puts "No file at #{filePath}"
+    exit 1
+end
+
 CSV.open(filePath, 'rb', headers: :first_row, encoding: 'UTF-8', col_sep: "\t") do |csv|
     currentModel = ''
-    csv.each do |row|
+    railsGstr = ''
+    line_count = File.readlines(filePath).size
+    linenum = 0
+    csv.read.each do |row|
+        linenum += 1
+        next if (row['model'].nil? or row['model'].empty?)
         if row['model'] != currentModel
-            railsGstr =
+            puts railsGstr if railsGstr != ''
+            currentModel = row['model']
+            railsGstr = "rails g #{currentModel} "
+        end
+
+        railsGstr += row['attribute'] + ':' + row['datatype'] + ' '
+
+        puts railsGstr if linenum == line_count
     end
 end
