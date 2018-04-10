a6ff2b191f3547eb282dc23c6cbff140a9f8ee22
Julien Lamarche <julien.lamarche@gmail.com>
2018-04-10 13:33:00 -0400

    Added git commenting for each file

--- gitForEachDiff.bash
+++ gitForEachDiff.bash
@@ -1,1 +1,12 @@
-gcd ; for file in `git status --porcelain  | grep " M" | cut -d ' ' -f 3 ` ;  do headline.bash "$file" ; git diff $file ; done
+gcd ;
+
+for file in `git status --porcelain  | grep " M" | cut -d ' ' -f 3 ` ;  do
+    headline.bash "$file" ;
+    git diff $file ;
+    echo ;
+    read -e -r -p "Git comment: " gitComment
+    set -x
+    git commit  $file  -m "$gitComment"
+    set +x
+    echo
+done
