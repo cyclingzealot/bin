28c63ec60cd6f57cdbad7b489583e9d7b5a1a772
Julien Lamarche <julien.lamarche@gmail.com>
2018-04-13 01:43:35 -0400

    added a counter in the prompt

--- gitForEachDiff.bash
+++ gitForEachDiff.bash
@@ -1,10 +1,17 @@
-gcd ;
+cd `git rev-parse --show-toplevel` ;
 
-for file in `git status --porcelain  | grep " M" | cut -d ' ' -f 3 ` ;  do
+set -x
+fileList=`git status --porcelain  | grep " M" | cut -d ' ' -f 3 `
+fileCount=`git status --porcelain | grep " M" | wc -l `
+set +x
+i=0
+
+for file in $fileList; do
+    let i++
     headline.bash "$file" ;
     git diff $file ;
     echo ;
-    read -e -r -p "Git comment: " gitComment
+    read -e -r -p "Git comment: ($i/$fileCount) " gitComment
     set -x
     git commit  $file  -m "$gitComment"
     set +x
