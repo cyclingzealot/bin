48a41ac50bc61f33520a07dfc9c3a1ea2dbc0cfe
Julien Lamarche <julien.lamarche@gmail.com>
2020-01-15 14:27:45 -0500

    Added outlookConvert.bash

---
+++ outlookConvert.bash
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+arg1=${1:-''}
+
+if [[ $arg1 == '--help' || $arg1 == '-h' ]]; then
+    echo "Script author should have provided documentation"
+    exit 0
+fi
+
+#exit when command fails (use || true when a command can fail)
+set -o errexit
+
+#exit when your script tries to use undeclared variables
+set -o nounset
+
+# in scripts to catch mysqldump fails
+set -o pipefail
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"   # Dir of the script
+__root="$(cd "$(dirname "${__dir}")" && pwd)"           # Dir of the dir of the script
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"       # Full path of the script
+__base="$(basename ${__file})"                          # Name of the script
+ts=`date +'%Y%m%d-%H%M%S'`
+ds=`date +'%Y%m%d'`
+pid=`ps -ef | grep ${__base} | grep -v 'vi ' | head -n1 |  awk ' {print $2;} '`
+formerDir=`pwd`
+
+# If you require named arguments, see
+# http://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+
+export DISPLAY=:0
+
+echo Begin `date`  .....
+
+echo; echo; echo;
+
+### BEGIN SCRIPT ###############################################################
+
+#(a.k.a set -x) to trace what gets executed
+#set -o xtrace
+
+echo "Installing packages libemail-outlook-message-perl libemail-sender-perl..."
+sudo apt-get install libemail-outlook-message-perl libemail-sender-perl
+
+baseName=`chopSuffix.bash $1`
+echo "Converting email..."
+perl -we 'use Email::Outlook::Message; print Email::Outlook::Message->new(shift)->to_email_mime->as_string' /tmp/Thinktel\ LCA\ footprint.msg  > "$baseName".eml
+
+set +x
+
+### END SCIPT ##################################################################
+
+cd $formerDir
