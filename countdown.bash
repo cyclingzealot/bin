42f511d6c6d2ab5a60488a93116e1771cf3193aa
Julien Lamarche <julien.lamarche@gmail.com>
2021-09-13 12:47:05 -0400

    Added \r in countdown so we don't loose sight of what we were doing.

--- countdown.bash
+++ countdown.bash
@@ -11,12 +11,27 @@ if [[ "$1" =~ $re ]]; then
     if [[ "$mins" == 'm' ]]; then
         secs=$((60*$secs))
     fi
-        
 
-	 for i in `seq $secs -1 0`; do printf "$i... " ; sleep 1; done
+
+	 for i in `seq $secs -1 0`; do
+     printf "$i... " ;
+     sleep 1;
+
+     if [ `expr $i % 15` -eq 0 ]; then
+        printf "       \r";
+     fi
+    done
 	exit 0
 fi
 
 echo 'No number specified... counting up....'
 i=0
-while(true); do printf "$i... " ; sleep 1; let i++; done
+while(true); do
+    printf "$i... " ;
+    sleep 1;
+    let i++;
+
+     if [ `expr $i % 15` -eq 0 ]; then
+        printf "       \r";
+     fi
+done
